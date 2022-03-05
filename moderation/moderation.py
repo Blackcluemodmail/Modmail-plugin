ebe1428feb3f1d1e1c54994005c51a34c9eaae59
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-03-05 22:58:23 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -3,7 +3,30 @@ import discord
 from discord.ext import commands
 from core import checks
 from core.models import PermissionLevel
+import traceback
+import sys
+import re
 
+time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
+time_dict = {"h":3600, "s":1, "m":60, "d":86400}
+
+class TimeConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        args = argument.lower()
+        matches = re.findall(time_regex, args)
+        time = 0
+        for v, k in matches:
+            try:
+                time += time_dict[k] * float(v)
+            except KeyError:
+                raise commands.BadArgument("{} is an invalid time-key! h/m/s/d are valid!".format(k))
+            except ValueError:
+                raise commands.BadArgument("{} is not a number!".format(v))
+        return time
+
+class MuteCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
 class Moderation(commands.Cog):
     """
