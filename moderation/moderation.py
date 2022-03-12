a78061c427d3a7075ba5c48a67847ee99b760aaf
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-03-12 22:16:38 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -6,7 +6,23 @@ from core.models import PermissionLevel
 import traceback
 import sys
 import re
-from core import time
+
+time_regex = re.compile("(?:(\d{1,5})(h|s|m|d))+?")
+time_dict = {"h":3600, "s":1, "m":60, "d":86400}
+
+class TimeConverter(commands.Converter):
+    async def convert(self, ctx, argument):
+        args = argument.lower()
+        matches = re.findall(time_regex, args)
+        time = 0
+        for v, k in matches:
+            try:
+                time += time_dict[k] * float(v)
+            except KeyError:
+                raise commands.BadArgument("{} is an invalid time-key! h/m/s/d are valid!".format(k))
+            except ValueError:
+                raise commands.BadArgument("{} is not a number!".format(v))
+        return time
 
 class MuteCog(commands.Cog):
     def __init__(self, bot):
@@ -266,7 +282,7 @@ class Moderation(commands.Cog):
 
     @commands.command(usage="<member> <duration> [reason]")
     @checks.has_permissions(PermissionLevel.MODERATOR)
-    async def mute(self, ctx, member: discord.Member = None, time:dateTime = None, *, reason=None):
+    async def mute(self, ctx, member: discord.Member = None, time:TimeConverter= None, *, reason=None):
         """Mutes the specified member, format should be in 1d, 10m, 10s."""
         if member == None:
             return await ctx.send_help(ctx.command)
