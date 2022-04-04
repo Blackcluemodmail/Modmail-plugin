415622d551f50154959a0e1ef2421eba899f977f
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-04 20:41:37 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -759,7 +759,7 @@ class Moderation(commands.Cog):
 
         current_nick = ctx.author
         await ctx.send(f"{ctx.author.mention}"
-        + (f" I have set your AFK: {reason}" if reason else "AFK"),
+        + (f" I have set your AFK: {reason}" if reason else "AFK")
 
         try:
             await ctx.author.edit(nick=f"[AFK] {ctx.author.name}")
