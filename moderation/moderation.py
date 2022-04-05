585e66af704ae098036c1cc1363597b8992464fc
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-05 18:13:23 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -136,33 +136,6 @@ class Moderation(commands.Cog):
             )
         )
 
-    @commands.command()
-    async def afk(ctx, *, reason=None):
-
-        if reason != None:
-            if not reason.endswith("."):
-                reason = reason + "."
-
-        current_nick = ctx.author
-
-        try:
-            await ctx.author.edit(nick=f"[AFK] {ctx.author.name}")
-        except discord.errors.Forbidden:
-            return await ctx.send(f"{ctx.author.mention}"
-        + (f" I have set your AFK: {reason}" if reason else "AFK")
-
-              int cou = 0
-              while int count <= int(mins)
-                 count += 1
-                 await asyncio.sleep(60)
-
-                if counter == int(mins):
-
-               try:
-                  await ctx.author.edit(nick=current_nick)
-               except discord.errors.Forbidden:
-                  return await ctx.send(f"Welcome back {ctx.author.mention}, I have removed your AFK")
-            
     @commands.command(usage="<member> [reason]")
     @checks.has_permissions(PermissionLevel.MODERATOR)
     async def warn(self, ctx, member: discord.Member = None, *, reason=None):
