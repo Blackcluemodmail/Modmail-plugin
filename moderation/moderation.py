31b690b73f283fa183eb7daf106f1f300ea505d7
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-24 23:46:26 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -364,15 +364,26 @@ class Moderation(commands.Cog):
             pass
 
         try:
-            await member.add_roles(role, reason=reason)
-        except discord.errors.Forbidden:
-            return await ctx.send(
-                embed=discord.Embed(
-                    title="Error",
-                    description="I don't have enough permissions to mute them.",
-                    color=discord.Color.red(),
-                ).set_footer(text="Please fix the permissions."), delete_after=30
-            )
+            if time= None:
+                await member.add_roles(role, reason=reason)
+            except discord.errors.Forbidden:
+                return await ctx.send(
+                    embed=discord.Embed(
+                        title="Error",
+                        description="I don't have enough permissions to mute them.",
+                        color=discord.Color.red(),
+                        ).set_footer(text="Please fix the permissions."), delete_after=30
+                     )
+            elif time != None:
+                await member.add_roles(role, reason=reason)
+            except discord.errors.Forbidden:
+                return await ctx.send(
+                    embed=discord.Embed(
+                        title="Error",
+                        description="I don't have enough permissions to mute them.",
+                        color=discord.Color.red(),
+                        ).set_footer(text="Please fix the permissions."), delete_after=30
+                     )
 
         case = await self.get_case()
 
