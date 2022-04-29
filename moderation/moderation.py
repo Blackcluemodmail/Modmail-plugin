071e3d50addcc39dfc72d18596bea798f085c0fb
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-30 02:52:34 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -3,6 +3,7 @@ import discord
 import typing
 from datetime import datetime
 from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
 from core import checks
 from core.models import PermissionLevel
 from core.utils import match_user_id
