403631ed6c40143c5b987e642d6cc2f3621ef7cc
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-30 09:18:34 +0530

    Update moderation.py

--- moderation/moderation.py
+++ moderation/moderation.py
@@ -189,7 +189,7 @@ class Moderation(commands.Cog):
             ).set_footer(text=f"This is the {case} case."), delete_after=10
         )
 
-    @bot.event() #Replace 'client' with whatever neccesary
+    @client.event()
     def on_command_error(ctx, error):
       if isinstance(error, commands.MissingRequiredArguments):
           # Replace MissingRequiredArguments with your error
