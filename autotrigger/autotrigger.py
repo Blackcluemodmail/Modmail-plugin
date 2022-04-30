c212fdc7854966b58083eb83939b09e4d8f1331e
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-30 19:06:45 +0530

    Update autotrigger.py

--- autotrigger/autotrigger.py
+++ autotrigger/autotrigger.py
@@ -1,16 +1,6 @@
-import asyncio
 import discord
 
 client = discord.Client()
-
-import typing
-from discord.ext import commands
-from discord.ext.commands import has_permissions, MissingPermissions
-from core import checks
-from core.utils import match_user_id
-import traceback
-import sys
-import re
 
 class Autotrigger(commands.Cog):
     def __init__(self, bot):
@@ -19,7 +9,7 @@ class Autotrigger(commands.Cog):
     @client.event
     async def on_message(message):
         if message.content.startswith('$hello'):
-            await ctx.send('Hello!')
+            await message.channel.send('Hello!')
 
 def setup(bot):
     bot.add_cog(Autotrigger(bot))
