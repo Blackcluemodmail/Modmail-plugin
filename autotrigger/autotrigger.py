cd6fcec3dde6bf30b6ac6bd4f8dc78ff9e5cac2d
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-04-30 18:42:00 +0530

    Update autotrigger.py

--- autotrigger/autotrigger.py
+++ autotrigger/autotrigger.py
@@ -13,7 +13,7 @@ class Autotrigger(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_message(message):
+    async def on_message(self, ctx):
         if message.content.startswith('yoo'):
             channel = message.channel
             await channel.send('Say hello!')
