12ce7d9ac06c5f7a6bfde8d237d10757cd3aec7e
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-05-01 14:50:15 +0530

    Update autotrigger.py

--- autotrigger/autotrigger.py
+++ autotrigger/autotrigger.py
@@ -1,16 +1,16 @@
+import asyncio
 import discord
 import typing
+from datetime import datetime
 from discord.ext import commands
-
-bot = commands.Bot('.')
-
 from discord.ext.commands import has_permissions, MissingPermissions
 from core import checks
+from core.models import PermissionLevel
 from core.utils import match_user_id
+from types import SimpleNamespace
 import traceback
 import sys
 import re
-import asyncio
 
 class AutoTrigger(commands.Cog):
     def __init__(self, bot):
