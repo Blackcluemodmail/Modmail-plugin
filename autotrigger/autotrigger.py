f17e3cf11598db75924ef4e49056c921bb329597
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-05-02 00:15:58 +0530

    Update autotrigger.py

--- autotrigger/autotrigger.py
+++ autotrigger/autotrigger.py
@@ -19,7 +19,7 @@ class AutoTrigger(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message):
         if int(667378631336525824) in message.content():
-            await message.channel.send(f"`CloudyOrk #ClueArmy` is AFK: kuchzada hi mobile chala liya aaj, purso ata hu ab")
+            await message.channel.send("`CloudyOrk #ClueArmy` is AFK: kuchzada hi mobile chala liya aaj, purso ata hu ab")
 
 
 def setup(bot):
