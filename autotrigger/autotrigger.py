17d313e53e50604a8f02d7f2bc8147c35a70d5a4
Blackcluemodmail <91614979+Blackcluemodmail@users.noreply.github.com>
2022-05-01 15:55:54 +0530

    Update autotrigger.py

--- autotrigger/autotrigger.py
+++ autotrigger/autotrigger.py
@@ -19,7 +19,7 @@ class AutoTrigger(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message, member: discord.Member=None):
         if message.author.id == bot.user.id:
-        return
+            return
 
         if "how to join smp" in message.content:
             await message.channel.send(f"{member.author.mention} ,check <#944498884351246336>", delete_after=5)
